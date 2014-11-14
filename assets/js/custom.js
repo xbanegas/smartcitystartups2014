924e73e720b8bedae180a397d28225c8afa218bc
Xavier Banegas <xbanegas@gmail.com>
2014-11-14 17:29:13 -0500

    seen-in feature first commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,14 @@
 (function() {
 
-$('#top-slider').height( $('#bgvid').height() );
+    function vidSize() {
+    	var sliderHeight = $('#top-slider').height(),
+    		vidHeight = $('#bgvid').height();
+    	
+    	if (sliderHeight != vidHeight) { 
+    		$('#top-slider').height(vidHeight) 
+    	};
+    };
+
+    setInterval(function(){vidSize()}, 1);
 
 })();
-
-
-    
